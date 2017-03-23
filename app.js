09da56bf2d47bcfb4e817cb7fa59d5e96002667d
Andrew Allshouse <allshouseandrew@aol.com>
2017-03-22 21:03:18 -0400

    get modal showing and log the result

--- app.js
+++ app.js
@@ -1,38 +1,108 @@
 (function(){
 
-    var app = angular.module('codeschool', ['ui.bootstrap.modal']);
+    // angular.module("uib/template/modal/window.html", []).run(["$templateCache", function($templateCache) {
+    //     $templateCache.put("uib/template/modal/window.html",
+    //         "<div class=\"modal-dialog {{size ? 'modal-' + size : ''}}\"><div class=\"modal-content\" uib-modal-transclude></div></div>\n" +
+    //         "");
+    // }]);
 
-    app.controller('ModalInstanceController', function($uibModalInstance){
-    });
+    // angular.module("uib/template/modal/myModalContent.html", []).run(["$templateCache", function($templateCache) {
+    //     $templateCache.put("uib/template/modal/myModalContent.html",
+    //         "<div class=\"modal-header\">\n" +
+    //             "<button type=\"button\" class=\"close\" ng-click=\"cancel()\">\n" +
+    //                 "<span aria-hidden=\"true\">&times;</span><span class=\"sr-only\">Close</span>\n" +
+    //             "</button>\n" +
+    //             "<h4 class=\"modal-title\" id=\"confirmDeactivateLabel\">Save Info</h4>\n" +
+    //         "</div>\n" +
+    //         "<div class=\"modal-body\">\n" +
+    //             "<p>Are you sure you want to save this?</p>\n" +
+    //         "</div>\n" +
+    //         "<div class=\"modal-footer\">\n" +
+    //             "<button class=\"btn btn-sm btn-default\" ng-click=\"modalCancel()\">\n" +
+    //                 "<i class=\"ace-icon fa fa-ban bigger-120\"></i>&nbsp;<strong>Cancel</strong>\n" +
+    //             "</button>\n" +
+    //             "<button class=\"btn btn-sm btn-success\" ng-click=\"modalSave()\">\n" +
+    //                 "<i class=\"ace-icon fa fa-check bigger-120\"></i>&nbsp;<strong>Save</strong>\n" +
+    //             "</button>\n" +
+    //         "</div>");
+    // }]);
 
-    app.controller('SelectChangeTestController', function($uibModal){
+    var app = angular.module('codeschool', [
+        'ui.bootstrap.modal'
+    ]);
+
+    app.controller('ModalInstanceController', ['$scope', function ($scope) {
+        // Cancel event
+        $scope.modalCancel = function () {
+            this.$dismiss('cancel');
+        };
+
+        // Discard changes and navigate to the desired location
+        $scope.modalSave = function () {
+            this.$close();
+        };
+    }]);
+
+    app.controller('SelectChangeTestController', ['$uibModal', '$templateCache', '$scope', function($uibModal, $templateCache, $scope){
+        $templateCache.put("uib/template/modal/window.html",
+            "<div class=\"modal-dialog {{size ? 'modal-' + size : ''}}\"><div class=\"modal-content\" uib-modal-transclude></div></div>\n" +
+            "");
+        $templateCache.put("uib/template/modal/myModalContent.html",
+            "<div class=\"modal-header\">\n" +
+                "<button type=\"button\" class=\"close\" ng-click=\"modalCancel()\">\n" +
+                    "<span aria-hidden=\"true\">&times;</span><span class=\"sr-only\">Close</span>\n" +
+                "</button>\n" +
+                "<h4 class=\"modal-title\" id=\"confirmDeactivateLabel\">Save Info</h4>\n" +
+            "</div>\n" +
+            "<div class=\"modal-body\">\n" +
+                "<p>Are you sure you want to save this?</p>\n" +
+            "</div>\n" +
+            "<div class=\"modal-footer\">\n" +
+                "<button class=\"btn btn-sm btn-default\" ng-click=\"modalCancel()\">\n" +
+                    "<i class=\"ace-icon fa fa-ban bigger-120\"></i>&nbsp;<strong>Cancel</strong>\n" +
+                "</button>\n" +
+                "<button class=\"btn btn-sm btn-success\" ng-click=\"modalSave()\">\n" +
+                    "<i class=\"ace-icon fa fa-check bigger-120\"></i>&nbsp;<strong>Save</strong>\n" +
+                "</button>\n" +
+            "</div>");
+
         var controller = this;
-        this.selectedRestaurant = 0;
+        $scope.previousSelectedRestaurant = 0;
+        $scope.selectedRestaurant = 0;
         
-        this.restaurants = [
+        $scope.restaurants = [
             { id: 1, name: "Moe's" },
             { id: 2, name: "Chipotle" },
             { id: 3, name: "5 Guys" },
             { id: 4, name: "Panera" }
         ];
 
-        this.loadInfo = function(){
-            if(controller.selectedRestaurant === "1"){
-                controller.city = "Bridgeville";
-                controller.state = "PA";
+        $scope.loadInfo = function(){
+            if($scope.selectedRestaurant === "1"){
+                $scope.city = "Bridgeville";
+                $scope.state = "PA";
             } else {
-                controller.city = "";
-                controller.state = "";
+                $scope.city = "";
+                $scope.state = "";
             }
+            $scope.previousSelectedRestaurant = $scope.selectedRestaurant;
         };
 
-        this.submit = function(){
+        $scope.submit = function(){
             console.log('button pressed');
-            $uibModal.open({
-                templateUrl: 'modalContent.html',
-                controller: 'ModalInstanceController'
+            var modalInstance = $uibModal.open({
+                template: $templateCache.get('uib/template/modal/myModalContent.html'),
+                //templateUrl: 'uib/template/modal/myModalContent.html',
+                controller: 'ModalInstanceController',
+                controllerAs: 'modalCtrl',
+                size: 'sm'
+            });
+            modalInstance.result.then(function(){
+                console.log('Success result from modal');
+            }, function(){
+                console.log('Cancel result from modal');
             });
         };
-    });
+    }]);
     
 })();
